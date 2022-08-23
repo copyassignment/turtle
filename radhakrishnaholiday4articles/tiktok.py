999f5e440520ac863d79e77a9eee2577b29df6bd
Ankur Gajurel <ankurgajurel02@gmail.com>
2022-08-23 21:40:25 +0545

    sachhai

--- radhakrishnaholiday4articles/tiktok.py
+++ radhakrishnaholiday4articles/tiktok.py
@@ -4,11 +4,11 @@ width(20) # width of the pen
 bgcolor('black') #background of the GUI
 rangharu= ['#db0f3c', '#50ebe7','white'] #all colors stored in a list 
 thaau = [(0,0), (-5,13), (-5,5)] # all coordinates stored in a list of tuples
-for (x,y),col in zip(thaau,rangharu): # loop through the colors and tuples at the same time
+for (x,y),kata in zip(thaau,rangharu): # loop through the colors and tuples at the same time
     up() 
     goto(x,y) # move the pen according to the current tuple in the loop 
     down()
-    color(col) # set the new color 
+    color(kata) # set the new color 
     left(180) # start drawing  
     circle(50, 270) # built-in function to draw a circle
     forward(120)
