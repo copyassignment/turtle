6d8dac56085622806980b9c964997f049ce2bd5b
Ankur Gajurel <ankurgajurel02@gmail.com>
2022-08-25 21:33:07 +0545

    yess

--- radhakrishnaholiday4articles/instagram.py
+++ radhakrishnaholiday4articles/instagram.py
@@ -1,8 +1,17 @@
 from turtle import * #importing the module
 
-bgcolor('#f3f3f3') #background of the GUI
-pencolor('#bc2a8d') #defining the color of the pen
-width(23) #defining the thickness of the pen
+## setting all the pre-defined values to be used in the program
+background_color = '#f3f3f3'
+pen_color = '#bc2a8d'
+width_val = 23
+round_coordA, round_coordB = 34, 90 
+circleAx, circleAy = 80, 360
+circleBx, circleBy = 7, 360
+gotoAx, gotoAy = 85, 30
+
+bgcolor(background_color) #background of the GUI
+pencolor(pen_color) #defining the color of the pen
+width(width_val) #defining the thickness of the pen
 penup() #start the draw
 goto(160,-100) #set the origin for the graphics
 pendown() #stop the draw temporarily
@@ -10,16 +19,16 @@ pendown() #stop the draw temporarily
 left(90) #turn at an angle 
 for i in range(4): #loop for 4 times for a square-type shape
     forward(250) 
-    circle(34,90) #for border-radius
+    circle(round_coordA, round_coordB) #for border-radius
 
 penup() # for the big circle
-goto(85,30) # defining the new origin for the pen
+goto(gotoAx, gotoAy) # defining the new origin for the pen
 pendown() # stopping the draw
-circle(80,360) #built-in function for a circle in the middle
+circle(circleAx, circleAy) #built-in function for a circle in the middle
 
 penup() 
 goto(110,130) # setting the new origin 
 pendown()
-circle(7,360) # built-in function for a circle at the top-right
+circle(circleBx, circleBy) # built-in function for a circle at the top-right
 
 done() # end the program
